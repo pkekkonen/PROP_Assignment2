7c54400f5612f7fe0728d8b6f59623c93ee54aa5
Ida <idso0102@dsv.su.se>
2019-12-09 14:50:35 +0100

    Hashtags added

--- safemacro/src/safemacro/core.clj
+++ safemacro/src/safemacro/core.clj
@@ -4,14 +4,19 @@
 (defmacro safe 
   ([expr]
    `(try 
-     ~(expr)
-     (catch Exception e (.getMessage e))))
+     ~expr
+     (catch Exception e# (.getMessage e#))))
   ([vector expr]
    ;; gör kontroll av längd på vector
-   (try
-     ~(let [var (first vector)
+   `(try
+     ~(let [^Closable var (first vector)
             value (first (rest vector))
             var value]
-        expr)
-     (catch Exception e (.getMessage e))
-     (finally (.close (^Closeable var))))))
+        expr
+        (. var close))
+     (catch Exception e# (.getMessage e#)))))
+
+
+(safe '([a 0] (/ 1 a)))
+(safe '(/ 1 0))
+(safe '(+ 1 2))
