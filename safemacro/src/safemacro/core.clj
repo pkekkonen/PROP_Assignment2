bf0a84e53bd8e12b3bbcea6b544a55e572c76712
pkekkonen <56582014+pkekkonen@users.noreply.github.com>
2019-12-09 14:07:20 +0100

    Started implementing safemacro

--- safemacro/src/safemacro/core.clj
+++ safemacro/src/safemacro/core.clj
@@ -2,12 +2,14 @@
   (:gen-class))
 
 (defmacro safe 
-  [vector expr]
-  (is (thrown? Exception (expr))))
-  
-  
-  
-  
-  
-  
-  
+  ([expr]
+   `(try 
+     ~(expr)
+     (catch Exception e (.getMessage e))))
+  ([vector expr]
+   ;; gör kontroll av längd på vector
+   (try
+     (let [var (first vector)
+           value (first (rest vector))
+           var value]
+       expr))))
