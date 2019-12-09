dd1e197466fd30f4267c0cccff65b8cce2608cf8
pkekkonen <56582014+pkekkonen@users.noreply.github.com>
2019-12-09 15:50:53 +0100

    Update core.clj

--- safemacro/src/safemacro/core.clj
+++ safemacro/src/safemacro/core.clj
@@ -1,6 +1,16 @@
 (ns safemacro.core
   (:gen-class))
 
+(import java.io.FileReader java.io.File)
+(import  java.io.Closeable)
+
+(def f (FileReader. (File. "ex.txt")))
+
+
+(def fr (new FileReader (File. "ex.txt")))
+
+(instance? Closeable var)
+
 (defmacro safe 
   ([expr]
    `(try 
@@ -9,17 +19,42 @@
   ([vector expr]
    ;; gör kontroll av längd på vector
    `(try
-     (let [^Closeable var ~(first vector)
-            value ~(first (rest vector))
-            var value]
-        (eval ~expr)
-        (. var close)
-      (catch Exception e# (.getMessage e#))))))
+     (let [^Closeable var (~(first vector))] (def f ~(first (rest vector))
+                                                  (let [var f]
+                                                    (eval ~expr)
+                                                    (if (instance? var Closeable)
+                                                      (. var close))
+                                                    (catch Exception e# (.getMessage e#))))))))
 
 
-(safe '([a 0] (/ 1 a)))
+(safe [a 0] (/ 1 a))
 
 (safe [s (FileReader. (File. "file.txt"))] (.read s))
 
 (safe '(/ 1 0))
 (safe '(+ 1 2))
+
+
+(macroexpand '(safe [s (FileReader. (File. "missing-file"))] (. s read)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
