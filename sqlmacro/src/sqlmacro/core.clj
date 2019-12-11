0ce451ac801d2b704f3a26cab8ff734e8c072e10
pkekkonen <56582014+pkekkonen@users.noreply.github.com>
2019-12-11 14:40:14 +0100

    Update core.clj

--- sqlmacro/src/sqlmacro/core.clj
+++ sqlmacro/src/sqlmacro/core.clj
@@ -1,5 +1,27 @@
 
-(defmacro select [columns _ table _ cond _ orderarg])
-  
+(defmacro select [wantedColumns _ table _ [condColumn condOp condValue] _ orderarg])
+
+
+(def entries [{:month 1 :val 12 :s1 true :s2 false}
+              {:month 2 :val 3 :s1 false :s2 true}
+              {:month 3 :val 32 :s1 true :s2 false}
+              {:month 4 :val 18 :s1 true :s2 false}
+              {:month 5 :val 32 :s1 false :s2 true}
+              {:month 6 :val 62 :s1 false :s2 true}
+              {:month 7 :val 12 :s1 false :s2 true}
+              {:month 8 :val 142 :s1 true :s2 false}
+              {:month 9 :val 52 :s1 true :s2 false}
+              {:month 10 :val 18 :s1 true :s2 false}
+              {:month 11 :val 23 :s1 false :s2 true}
+              {:month 12 :val 56 :s1 false :s2 true}])
+
+(defn getColumns [table]
+  (for [x (range 0 (count table))] 
+    (select-keys (get table x) [:val :s2])))
+
+(getColumns entries)
+
+(defn filterCond [table condOp condColumn condValue]
+  )
 
 ;; (filter (function) listan)
