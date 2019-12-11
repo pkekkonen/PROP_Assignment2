99ae0d15cdf207039e0c636b1442a7eadf18542e
pkekkonen <56582014+pkekkonen@users.noreply.github.com>
2019-12-11 15:44:25 +0100

    Update core.clj

--- sqlmacro/src/sqlmacro/core.clj
+++ sqlmacro/src/sqlmacro/core.clj
@@ -1,6 +1,14 @@
+(def <> not=)
 
-(defmacro select [wantedColumns _ table _ [condColumn condOp condValue] _ orderarg])
+(def persons '({:id 1 :name "olle"} {:id 2 :name "anna"} {:id 3 :name "isak"} {:id 4 :name "beatrice"}))
 
+(select [:id :name] from persons where [:id > 2] orderby :name) 
+
+(defmacro select [[& wantedColumns] _ table _ [condColumn condOp condValue] _ orderArg]
+  `(orderTable
+    (filterCond
+      ((getColumns ~table ~wantedColumns) ~condOp ~condColumn ~condValue))
+    ~orderArg))
 
 (def entries [{:month 1 :val 12 :s1 true :s2 false}
               {:month 2 :val 3 :s1 false :s2 true}
@@ -15,15 +23,20 @@
               {:month 11 :val 23 :s1 false :s2 true}
               {:month 12 :val 56 :s1 false :s2 true}])
 
-(defn getColumns [table]
+(defn getColumns [table & wantedColumns]
   (for [x (range 0 (count table))] 
-    (select-keys (get table x) [:val :s2])))
+    (select-keys (get table x) wantedColumns)))
 
-(getColumns entries)
+(getColumns entries :val :s2)
 
 (defn filterCond [table condOp condColumn condValue]
   (filter #(condOp (condColumn %) condValue) table))
 
-(filterCond (getColumns entries) = :s2 true)
+(filterCond (getColumns entries :val :s2) <> :val 3)
+
+(defn orderTable [table orderArg]
+  (sort-by orderArg  table))
+
+(orderTable (filterCond (getColumns entries :val :s2) = :s2 true) :val)
 
 ;; (filter (function) listan)
