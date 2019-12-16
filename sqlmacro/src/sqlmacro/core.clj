6d4228f013a104d8e60a930d3477f6443493a59d
Ida <idso0102@dsv.su.se>
2019-12-16 12:04:27 +0100

    Update core.clj

--- sqlmacro/src/sqlmacro/core.clj
+++ sqlmacro/src/sqlmacro/core.clj
@@ -1,19 +1,21 @@
 (def <> not=)
-
-(def persons '({:id 1 :name "olle"} {:id 2 :name "anna"} {:id 3 :name "isak"} {:id 4 :name "beatrice"}))
-
-(macroexpand '(select [:id :name] from persons where [:id > 2] orderby :name)) 
-
-(select [:id :name] from persons where [:id > 2] orderby :name) 
-
-(select [:s2 :s1] from entries where [:month > 4] orderby :val) 
-
+(import java.util.Comparator)
 
 (defmacro select [[& wantedColumns] _ table _ [condColumn condOp condValue] _ orderArg]
-  `(orderTable
-    (filterCond
-      ((getColumns ~table ~wantedColumns)  ~condOp ~condColumn ~condValue))
-    ~orderArg))
+
+  (defn getColumns [table wantedColumns]
+    (select-keys table wantedColumns))
+  
+  (defn filterCond [table condOp condColumn condValue]
+    (filter #(condOp (condColumn %) condValue) table))
+  
+  (defn orderTable [table orderArg]
+    (sort-by orderArg table))
+  
+  `(getColumns ~table ~wantedColumns))
+  
+   
+  
 
 (def entries [{:month 1 :val 12 :s1 true :s2 false}
               {:month 2 :val 3 :s1 false :s2 true}
@@ -28,30 +30,19 @@
               {:month 11 :val 23 :s1 false :s2 true}
               {:month 12 :val 56 :s1 false :s2 true}])
 
-(defn getColumns 
-  ([table wantedColumns]
-   (if (empty? (rest table))
-     (conj nil (select-keys (first table) wantedColumns))
-     (getColumns table nil wantedColumns)))
-  ([table result wantedColumns]
-   (if (empty? (rest table))
-     (conj result (select-keys (first table) wantedColumns))
-     (getColumns (rest table) (conj result (select-keys (first table) wantedColumns)) wantedColumns))))
+(def persons [{:id 1 :name "olle"} 
+              {:id 2 :name "anna"} 
+              {:id 3 :name "isak"} 
+              {:id 4 :name "beatrice"}])
 
-(defn getColumnsOldSolution [table & wantedColumns]
-  (for [x (range 0 (count table))] 
-    (select-keys (get table x) wantedColumns)))
+(macroexpand '(select [:id :name] from persons where [:id > 2] orderby :name)) 
 
-(getColumns entries '(:val))
+(select [:id :name] from persons where [:id > 2] orderby :name) 
 
-(defn filterCond [table condOp condColumn condValue]
-  (filter #(condOp (condColumn %) condValue) table))
+(select [:s2 :s1] from entries where [:month > 4] orderby :val)
 
-(filterCond (getColumns entries '(:val :s2)) <> :val 3)
+(select [:s2] from entries where [:month > 4] orderby :val)
 
-(defn orderTable [table orderArg]
-  (sort-by orderArg table))
-
-(orderTable (filterCond (getColumns entries '(:val :s2)) = :s2 true) :val)
+(macroexpand '(select [:s2 :s1] from entries where [:month > 4] orderby :val))
 
 ;; (filter (function) listan)
