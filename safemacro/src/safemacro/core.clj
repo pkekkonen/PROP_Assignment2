f146cd042520855a226650641b209b50c817460f
Ida <idso0102@dsv.su.se>
2019-12-16 14:41:13 +0100

    changed getmessage to str

--- safemacro/src/safemacro/core.clj
+++ safemacro/src/safemacro/core.clj
@@ -8,23 +8,23 @@
   ([expr]
    `(try 
      (eval ~expr)
-     (catch Exception e# (.getMessage e#))))
+     (catch Exception e# (str e#))))
   ([[^Closeable var value] expr]
    `(try
      (let [~var ~value]
       (eval ~expr))
-     (catch Exception e#  (.getMessage e#)) ;;ska returnera e
+     (catch Exception e# (str e#)) ;;ska returnera e
      (finally 
-      (let [~var ~value]
-       (if (instance? Closeable ~var)
-           (. ~var close)))))))
+       (let [~var ~value]
+         (if (instance? Closeable ~var)
+          (. ~var close)))))))
 
 (macroexpand '(safe [a 0] a))
 (safe [a 0] (/ 1 a))
 (safe [a 2] (+ a 22))
+(safe (/ 2 0))
 (macroexpand '(safe [a 2] (+ a 22)))
 
-(def v (safe [s (FileReader. (File. "hejhej.txt"))] (.read s)))
+(safe [s (FileReader. (File. "hejhej.txt"))] (.read s))
 (macroexpand '(safe [s (FileReader. (File. "hejhej.txt"))] (.read s)))
 
-(do v)
