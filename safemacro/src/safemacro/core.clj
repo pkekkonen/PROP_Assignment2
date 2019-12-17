186226099ac7a199b531a11c4f9e050a2bc74d99
pkekkonen <56582014+pkekkonen@users.noreply.github.com>
2019-12-17 17:08:48 +0100

    Implemented main functions

--- safemacro/src/safemacro/core.clj
+++ safemacro/src/safemacro/core.clj
@@ -8,23 +8,35 @@
   ([expr]
    `(try 
      (eval ~expr)
-     (catch Exception e# (str e#))))
+     (catch Exception e# e#)))
   ([[^Closeable var value] expr]
-   `(try
-     (let [~var ~value]
-      (eval ~expr))
-     (catch Exception e# (str e#)) ;;ska returnera e
-     (finally 
-       (let [~var ~value]
-         (if (instance? Closeable ~var)
-          (. ~var close)))))))
+   `(try    
+     (let [~var ~value res# (eval ~expr)]
+  
+      (if (instance? Closeable ~var)
+       (. ~var close))
+      res#)
+     (catch Exception e# e#)))) ;;ska returnera e
 
-(macroexpand '(safe [a 0] a))
-(safe [a 0] (/ 1 a))
-(safe [a 2] (+ a 22))
-(safe (/ 2 0))
-(macroexpand '(safe [a 2] (+ a 22)))
+(defn -main [& args]
+  
+  
+  (def f(safe (+ 1 10)))
+  (println f)
+  
+  (def p(safe [a 2] (+ a 22)))
+  (println p)
+  
+  (def g(safe (/ 2 0)))
+  (println g)
+  
+  (def v (safe [s (FileReader. (File. "/Users/paulinakekkonen/Desktop/hejhej.txt"))] (.read s)))
+  (println v)
+  
+  (def w(safe [s (FileReader. (File. "dhejhej.txt"))] (.read s)))
+  (println w)) 
 
-(safe [s (FileReader. (File. "hejhej.txt"))] (.read s))
-(macroexpand '(safe [s (FileReader. (File. "hejhej.txt"))] (.read s)))
+
+
+
 
--- sqlmacro/src/sqlmacro/core.clj
+++ sqlmacro/src/sqlmacro/core.clj
@@ -1,24 +1,5 @@
-(def <> not=)
-
-(def entries '({:month 1 :val 12 :s1 true :s2 false}
-               {:month 2 :val 3 :s1 false :s2 true}
-               {:month 3 :val 32 :s1 true :s2 false}
-               {:month 4 :val 18 :s1 true :s2 false}
-               {:month 5 :val 32 :s1 false :s2 true}
-               {:month 6 :val 62 :s1 false :s2 true}
-               {:month 7 :val 12 :s1 false :s2 true}
-               {:month 8 :val 142 :s1 true :s2 false}
-               {:month 9 :val 52 :s1 true :s2 false}
-               {:month 10 :val 18 :s1 true :s2 false}
-               {:month 11 :val 23 :s1 false :s2 true}
-               {:month 12 :val 56 :s1 false :s2 true}))
-
-(def persons '({:id 1 :name "olle"} 
-               {:id 2 :name "anna"} 
-               {:id 3 :name "isak"} 
-               {:id 4 :name "beatrice"}))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(ns sqlmacro.core
+  (:gen-class))
 
 
 (defmacro select [[& wantedColumns] _ table _ [condColumn condOp condValue] _ orderArg]
@@ -31,33 +12,38 @@
     (filter #(~condOp (~condColumn %) ~condValue) (sort-by ~orderArg ~table)) []))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(select [:id :name] from persons where [:id < 9] orderby :id) 
-
-(select [:month] from entries where [:s1 = true] orderby :val)
 
 
 
+(defn -main [& args]
+  (def <> not=)
+  (def entries '({:month 1 :val 12 :s1 true :s2 false}
+                 {:month 2 :val 3 :s1 false :s2 true}
+                 {:month 3 :val 32 :s1 true :s2 false}
+                 {:month 4 :val 18 :s1 true :s2 false}
+                 {:month 5 :val 32 :s1 false :s2 true}
+                 {:month 6 :val 62 :s1 false :s2 true}
+                 {:month 7 :val 12 :s1 false :s2 true}
+                 {:month 8 :val 142 :s1 true :s2 false}
+                 {:month 9 :val 52 :s1 true :s2 false}
+                 {:month 10 :val 18 :s1 true :s2 false}
+                 {:month 11 :val 23 :s1 false :s2 true}
+                 {:month 12 :val 56 :s1 false :s2 true}))
+
+  (def persons '({:id 1 :name "olle"} 
+                 {:id 2 :name "anna"} 
+                 {:id 3 :name "isak"} 
+                 {:id 4 :name "beatrice"}))
+  
+  (def v(select [:id :name] from persons where [:id < 9] orderby :id)) 
+  (println v)
+  (def f(select [:month] from entries where [:s1 = true] orderby :val))
+  (println f))
 
 
-(defn getColumns2 [wantedColumns table returnValue] 
-    (if (empty? (rest table))
-      (seq (conj returnValue (select-keys (first table) wantedColumns)))
-      (recur wantedColumns (rest table) (conj returnValue (select-keys (first table) wantedColumns)))))
 
 
 
 
-(defn select2 [[& wantedColumns] table condColumn condOp condValue orderArg]
-  (getColumns2 wantedColumns
-    (filter #(condOp (condColumn %) condValue) (sort-by orderArg table)) '[]))
-
-(select2 [:id :name] persons :id < 9 :id) 
-
-(select2 [:month] entries :s1 = true :val)
-
-(getColumns2 '[:month]  entries '[])
 
 
