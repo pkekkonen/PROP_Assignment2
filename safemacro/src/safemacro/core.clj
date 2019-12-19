d48a626addcc16e49d1e9046c31f8e338b0946f8
Ida <idso0102@dsv.su.se>
2019-12-19 15:52:26 +0100

    Update core.clj

--- safemacro/src/safemacro/core.clj
+++ safemacro/src/safemacro/core.clj
@@ -4,8 +4,13 @@
 (import java.io.FileReader java.io.File)
 (import java.io.Closeable)
 
-(defmacro safe 
+(def ^:dynamic var2 0)
+(def ^:dynamic value2 0)
+(def ^:dynamic res 0)
+
+(defn safe 
   ([expr]
+<<<<<<< Updated upstream
    `(try 
      (eval ~expr)
      (catch Exception e# (.getMessage e#))))
@@ -28,3 +33,38 @@
 (macroexpand '(safe [s (FileReader. (File. "hejhej.txt"))] (.read s)))
 
 (do v)
+=======
+   (try 
+     (eval expr)
+     (catch Exception e (str e))))
+  ([[^Closeable var value] expr]
+   (try    
+     (binding [var2 var 
+               value2 value 
+               var2 value2 
+               res (eval expr)]
+       
+       (if (instance? Closeable var)
+         (. var close))
+       res)
+     (catch Exception e (str e))))) ;;ska returnera e
+
+  
+(safe (+ 1 10))
+  
+(safe [a 2] (+ a 22)
+  (println p))
+  
+(safe (/ 2 0)
+  (println g))
+  
+(safe [s (FileReader. (File. "/Users/paulinakekkonen/Desktop/hejhej.txt"))] (.read s)
+  (println v))
+  
+(safe [s (FileReader. (File. "dhejhej.txt"))] (.read s)
+  (println w)) 
+
+
+
+
+>>>>>>> Stashed changes
