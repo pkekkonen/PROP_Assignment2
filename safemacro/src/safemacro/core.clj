dde0a662453c664059a3c8ecd8e7cbc623259cc6
pkekkonen <paulina.kekkonen@hotmail.com>
2019-12-19 16:26:36 +0100

    Finish

--- safemacro/src/safemacro/core.clj
+++ safemacro/src/safemacro/core.clj
@@ -16,27 +16,6 @@
       (if (instance? Closeable ~var)
        (. ~var close))
       res#)
-     (catch Exception e# e#)))) ;;ska returnera e
-
-(defn -main [& args]
-  
-  
-  (def f(safe (+ 1 10)))
-  (println f)
-  
-  (def p(safe [a 2] (+ a 22)))
-  (println p)
-  
-  (def g(safe (/ 2 0)))
-  (println g)
-  
-  (def v (safe [s (FileReader. (File. "/Users/paulinakekkonen/Desktop/hejhej.txt"))] (.read s)))
-  (println v)
-  
-  (def w(safe [s (FileReader. (File. "dhejhej.txt"))] (.read s)))
-  (println w)) 
-
-
-
+     (catch Exception e# e#))))
 
 
--- sqlmacro/src/sqlmacro/core.clj
+++ sqlmacro/src/sqlmacro/core.clj
@@ -15,35 +15,5 @@
 
 
 
-(defn -main [& args]
-  (def <> not=)
-  (def entries '({:month 1 :val 12 :s1 true :s2 false}
-                 {:month 2 :val 3 :s1 false :s2 true}
-                 {:month 3 :val 32 :s1 true :s2 false}
-                 {:month 4 :val 18 :s1 true :s2 false}
-                 {:month 5 :val 32 :s1 false :s2 true}
-                 {:month 6 :val 62 :s1 false :s2 true}
-                 {:month 7 :val 12 :s1 false :s2 true}
-                 {:month 8 :val 142 :s1 true :s2 false}
-                 {:month 9 :val 52 :s1 true :s2 false}
-                 {:month 10 :val 18 :s1 true :s2 false}
-                 {:month 11 :val 23 :s1 false :s2 true}
-                 {:month 12 :val 56 :s1 false :s2 true}))
-
-  (def persons '({:id 1 :name "olle"} 
-                 {:id 2 :name "anna"} 
-                 {:id 3 :name "isak"} 
-                 {:id 4 :name "beatrice"}))
-  
-  (def v(select [:id :name] from persons where [:id < 9] orderby :id)) 
-  (println v)
-  (def f(select [:month] from entries where [:s1 = true] orderby :val))
-  (println f))
-
-
-
-
-
-
 
 
